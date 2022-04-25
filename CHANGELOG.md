beaf1a1381d0d56610e87e74369d84346b134479
Harsh Chhikara <harshchhikara1993@gmail.com>
2022-04-25 16:32:33 +0530

    initial changes

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## 0.0.1
+
+0.0.1-First Release
+A package that helps you to create random noise patterns in flutter.
+Only Android support as of now.
---
+++ lib/noisegenerationcode.dart
@@ -0,0 +1,80 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class NoiseBox extends StatefulWidget {
+  @override
+  State<NoiseBox> createState() => _NoiseBoxState();
+}
+
+class _NoiseBoxState extends State<NoiseBox> with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  //dimension of noise box
+  double _WIDTH = 100.0;
+  double _HEIGHT = 100.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(vsync: this, duration: Duration(seconds: 5),);
+
+    _animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Container(
+          color: Colors.white,
+          width: _WIDTH,
+          height: _HEIGHT,
+          child: AnimatedBuilder(animation: _animationController, builder: (BuildContext ctx, Widget w){
+            return CustomPaint(
+              painter: NoisePainter(
+                width: _WIDTH,
+                height: _HEIGHT,
+              ),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
+
+class NoisePainter extends CustomPainter{
+
+  final double width;
+  final double height;
+
+  NoisePainter({this.width, this.height});
+
+  List<Color> colors = [Colors.red, Colors.black, Colors.grey, Colors.purple,Colors.green];
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Random random = Random();
+
+    for(int i =0 ;i <this.width;i+=5){
+      for(int j =0 ;j <this.height;j+=5){
+        Offset point = Offset(
+          random.nextDouble() * (this.width),//getting random width
+          random.nextDouble() * (this.height),//getting random height
+        );
+        Paint p = Paint();
+        p.color = colors[random.nextInt(colors.length)];
+        p.strokeWidth = random.nextDouble() * 2;
+
+        canvas.drawPoints(PointMode.points, [point], p);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+
+}
